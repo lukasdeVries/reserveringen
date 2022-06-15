7d49fd3ec9f8dd68640f6625b2037db2e5d458ca
lukasdeVries <lukaspdevries@gmail.com>
2022-06-15 14:09:21 +0200

    header en index

--- index.php
+++ index.php
@@ -5,8 +5,13 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Snelkookpan</title>
+    <link rel="stylesheet" href="css/main.css">
 </head>
 <body>
-    
+    <div class="wrapper">
+        <?php require_once "header.php" ?>
+    </div>
+    <div class="forrest-image"></div>
+
 </body>
 </html>
